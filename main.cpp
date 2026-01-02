039f5a7151fd2b850b1ab4869f5b0a6a358b5f53
licht <3210105158@zju.edu.cn>
2026-01-02 16:01:33 +0800

    task1: line drawing algorithm_imple

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <random>
 #include "tgaimage.h"
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
@@ -6,19 +7,44 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+void line(int x0, int y0, int x1, int y1, TGAImage &framebuffer, TGAColor color);
+
 int main(int argc, char** argv) {
     constexpr int width  = 64;
     constexpr int height = 64;
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
-
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
+    std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+    for (int i = 0; i < (1 << 24); i++) {
+        int x0 = rng() % width, y0 = rng() % height;
+        int x1 = rng() % width, y1 = rng() % height;
+        line(x0, y0, x1, y1, framebuffer, {static_cast<uint8_t>(rng() % 256), 
+        static_cast<uint8_t>(rng() % 256), 
+        static_cast<uint8_t>(rng() % 256), 
+        static_cast<uint8_t>(rng() % 256)});
+    }
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
+}
+
+void line(int x0, int y0, int x1, int y1, TGAImage &framebuffer, TGAColor color) {
+    int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+    int dy = -abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+    int err = dx + dy, e2;
+
+    while (true) {
+        framebuffer.set(x0, y0, color);
+        if (x0 == x1 && y0 == y1) break;
+        
+        e2 = 2 * err;
+        if (e2 >= dy) {
+            err += dy;
+            x0 += sx;
+        }
+        if (e2 <= dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
 }
